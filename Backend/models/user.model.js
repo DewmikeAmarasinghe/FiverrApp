cc072cc7aab9f0f04565e00bc09e6be907f495a8
DewmikeAmarasinghe <dewmikela@gmail.com>
2025-07-15 10:57:00 +0530

    feat(backend/models): add user.model.js

---
+++ Backend/models/user.model.js
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: false,
+        default: 'https://www.svgrepo.com/show/122119/user-image-with-black-background.svg'
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: false,
+    },
+    desc: {
+        type: String,
+        required: false,
+    },
+    isSeller: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    timestamps: true
+});
+
+export default model("User", userSchema);
